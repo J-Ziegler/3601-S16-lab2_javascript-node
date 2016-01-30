8b0eb8284817a7b17e721111d3cd6343afeaf06b
J-Ziegler <ziegl210@morris.umn.edu>
2016-01-30 14:15:00 -0600

    More work on GPA Calculator function. Wrote tests for letter grade to number converter.

--- public/javascript/clientJavascript.js
+++ public/javascript/clientJavascript.js
@@ -31,8 +31,9 @@ var returnKittens = function(){
 
 
 // Lots of cases? Sure. Necessary? No. Worth it? Really also no.
-var leetify = function(s){
+var leetify = function(s){ // s is a String.
     var str = s.split('');
+
     for (var i = 0; i < str.length; i++){
         if(str[i] == 'a' || str[i] == 'A'){
             str[i] = '@';
--- public/javascript/clientJavascript.spec.js
+++ public/javascript/clientJavascript.spec.js
@@ -24,5 +24,35 @@ describe('testing basic functions', function(){
         expect(leetify("JoeThe")).toBe("J037#3");
         expect(leetify("abcdefghijklmnopqrstuvwxyz")).toBe("@8(d3f6#!jk1mn0pqr$7uvwxy2");
         expect(leetify("ABCDEFGHIJKLMNOPQRSTUVWXYZ")).toBe("@8(D3F6#!JK1MN0PQR$7UVWXY2");
-    })
+    });
+
+
+    // Testing for the GPA Calculator.
+    it('Calculate the GPA', function(){
+        var calculateGPA = function(gradeOne, creditsOne, gradeTwo, creditsTwo, gradeThree, creditsThree){
+
+        };
+
+
+    });
+
+    it('Testing letter to number conversions', function(){
+        var letToNum = function(letGrade){
+
+        };
+
+        expect(letToNum("A")).toBe(4.00);
+        expect(letToNum("A-")).toBe(3.66);
+        expect(letToNum("B+")).toBe(3.33);
+        expect(letToNum("B")).toBe(3.00);
+        expect(letToNum("B-")).toBe(2.66);
+        expect(letToNum("C+")).toBe(2.33);
+        expect(letToNum("C")).toBe(2.00);
+        expect(letToNum("C-")).toBe(1.66);
+        expect(letToNum("D+")).toBe(1.33);
+        expect(letToNum("D")).toBe(1.00);
+        expect(letToNum("D-")).toBe(0.66);
+        expect(letToNum("F")).toBe(0);
+        expect(letToNum("sdf")).toThrowError(InvalidParamsError)
+    });
 });
