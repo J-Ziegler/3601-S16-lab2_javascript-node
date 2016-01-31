ca89dab4448c09a9926b186f707920cabdb51448
J-Ziegler <ziegl210@morris.umn.edu>
2016-01-31 02:08:19 -0600

    GPA Calculator moved into client-side JS for increased neatness. Still located on the server-side for our Lab, but this is so the tests work.

--- public/javascript/clientJavascript.js
+++ public/javascript/clientJavascript.js
@@ -64,3 +64,57 @@ var leetify = function(s){ // s is a String.
 
     return str.join('');
 };
+
+
+// GPA Calculator below this point
+
+
+var letToNum = function(letGrade){ // Yes,it's redundant. But... Reasons. I'll probably fix it later, add it to the tech debt, whatever.
+    //if (!letGrade.isString) {
+    //    throw InvalidParamsError();
+    //}
+    var letterGrade = letGrade.toUpperCase();
+
+    if (letterGrade == "A") {
+        return 4.00;
+    } else if (letterGrade == "A-"){
+        return 3.66;
+    } else if (letterGrade == "B+"){
+        return 3.33;
+    } else if (letterGrade == "B"){
+        return 3.00;
+    } else if (letterGrade == "B-"){
+        return 2.66;
+    } else if (letterGrade == "C+"){
+        return 2.33;
+    } else if (letterGrade == "C"){
+        return 2.00;
+    } else if (letterGrade == "C-"){
+        return 1.66;
+    } else if (letterGrade == "D+"){
+        return 1.33;
+    } else if (letterGrade == "D"){
+        return 1.00;
+    } else if (letterGrade == "D-"){
+        return 0.66;
+    } else if (letterGrade == "F") {
+        return 0;
+    } else {
+        return -1;
+    }
+};
+
+var calculateGPA = function(gradeOne, firstCredits, gradeTwo, secondCredits, gradeThree, thirdCredits){
+    var firstGrade = letToNum(gradeOne);
+    var secondGrade = letToNum(gradeTwo);
+    var thirdGrade = letToNum(gradeThree);
+
+
+    if (firstGrade === -1 || secondGrade === -1 || thirdGrade === -1) {
+        return "Error, you gave us an invalid grade!";
+    }
+
+    return ((firstGrade * firstCredits) +
+        (secondGrade * secondCredits) +
+        (thirdGrade * thirdCredits)) / (parseInt(firstCredits) + parseInt(secondCredits) + parseInt(thirdCredits));
+};
